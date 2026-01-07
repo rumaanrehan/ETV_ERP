0d7b03427ab60a7918a78719e5ef937636b46c09
Abdul Hameed <abdulhameed8888khan@gmail.com>
2026-01-07 20:20:37 +0530

    By Abdul Hameed on 2026-01-07 20:20

--- src/app/shared/routes/main.routes.ts
+++ src/app/shared/routes/main.routes.ts
@@ -9,7 +9,7 @@ export const mainRoute: Routes = [
   { path: 'server-error', component: ServerErrorComponent },
   { path: 'profile', loadComponent: () => import('../../../app/components/profile/profile/profile.component').then((m) => m.ProfileComponent) },
   { path: 'dashboard', loadComponent: () => import('../../../app/modules/dashboard/dashboard.component').then((m) => m.DashboardComponent) },
-    { path: 'organisation-setting', loadComponent: () => import('../../../app/components/organization-setting/organization-setting.component').then((m) => m.OrganizationSettingComponent) },
+  { path: 'organisation-setting', loadComponent: () => import('../../../app/components/organization-setting/organization-setting.component').then((m) => m.OrganizationSettingComponent) },
   { path: '', loadChildren: () => import('../../modules/admin/admin.routes').then((m) => m.adminRoute) },
   { path: '', loadChildren: () => import('../../modules/ims/ims.routes').then((m) => m.imsRoute) },
   { path: '', loadChildren: () => import('../../modules/ie/ie.routes').then((m) => m.ieRoute) }
