34f62211dd5925f899c46ce5f7574392b0ca8191
Abdul Hameed <abdulhameed8888khan@gmail.com>
2026-01-29 21:18:38 +0530

    By Abdul Hameed on 2026-01-29 21:12

--- src/app/modules/ims/settings/product-master/product-master.ts
+++ src/app/modules/ims/settings/product-master/product-master.ts
@@ -1,25 +1,19 @@
 export interface ProductMaster {
   ProductID: number | null;
   ProductCode: string | null;
-  // ItemTypeID: number | null;
-  // ItemGroupID: number | null;
   ItemCategoryID: number | null;
   ProductName: string | null;
-  // GenericID: number | null;
-  // ManufacturerID: number | null;
   UOMID: number | null;
   Unit: number | null;
   HSCode: string | null;
   TaxSlabID: number | null;
-  PurTaxRate: number | null;
-  // ReorderLevel: number | null;
-  // ReorderQty: number | null;
-  // IsApprovalRequiredToPurchase: boolean | null;
-  // IsApprovalRequiredToIssue: boolean | null;
-  NetWeight?: number | null;
-  GrossWeight?: number | null;
-  ProductDescription?: string | null;
   PurTaxOn: string | null;
+}
+
+export interface ProductRequest {
+  ProductCode?: string | null;
+  ProductName?: string | null;
+  PopulateType?: string | null;
 }
 
 export interface Product_SelectList {
@@ -30,54 +24,32 @@ export interface Product_SelectList {
   PurTaxRate: number | null;
 }
 
-export interface ProductMaster_IndexTableFilter {
+export interface Product_IndexTableFilter {
   ProductCode: string | null;
   ProductName: string | null;
-  ItemGroupName: string | null;
   ItemCategoryName: string | null;
-  GenericItemName: string | null;
-  ManufacturerName: string | null;
   UOMName: string | null;
   ActiveStatusID: number | null;
 }
 
-export interface ProductMaster_IndexTableList {
+export interface Product_IndexTableList {
   RowID: number;
   ProductID: number;
   ProductCode: string;
   ProductName: string;
-  ItemGroupName: string;
   ItemCategoryName: string;
-  GenericItemName: string;
-  ManufacturerName: string;
   UOMName: string;
-  UnitPrice: number;
   ActiveStatus: boolean;
-}
-
-export interface ProductRequest {
-  ProductCode?: string | null;
-  ProductName?: string | null;
-  PopulateType?: string | null;
 }
 
 export interface Product_Details {
   ProductID: number | null;
   ProductCode: string | null;
-  ItemTypeID: number | null;
-  ItemGroupID: number | null;
   ItemCategoryID: number | null;
   ProductName: string | null;
-  GenericID: number | null;
-  ManufacturerID: number | null;
   UOMID: number | null;
   Unit: number | null;
   HSCode: string | null;
   TaxSlabID: number | null;
-  PurTaxRate: number | null;
-  ReorderLevel: number | null;
-  ReorderQty: number | null;
-  IsApprovalRequiredToPurchase: boolean | null;
-  IsApprovalRequiredToIssue: boolean | null;
   PurTaxOn: string | null;
 }
